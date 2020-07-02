dd6e52d9ddd4b9fc2c746c2c53b3206e6cda4888
Juan Cleiton <cleiton.duraes@clicksoft.com.br>
2020-07-01 23:20:12 -0300

    FIX: Alter gesture handler in MainActivity

--- android/app/src/main/java/com/nubank/MainActivity.java
+++ android/app/src/main/java/com/nubank/MainActivity.java
@@ -15,4 +15,14 @@ public class MainActivity extends ReactActivity {
   protected String getMainComponentName() {
     return "nubank";
   }
+
+  @Override
+  protected ReactActivityDelegate createReactActivityDelegate() {
+    return new ReactActivityDelegate(this, getMainComponentName()) {
+      @Override
+      protected ReactRootView createRootView() {
+      return new RNGestureHandlerEnabledRootView(MainActivity.this);
+      }
+    };
+  }
 }
